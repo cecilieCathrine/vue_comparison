7866da6a6c50819865d527d8d462bee0dc2381a1
cecilieCathrine <cecilie@kraftlauget.no>
2020-01-05 16:35:43 +0100

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WhereToGo msg="Help! Where to go?"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WhereToGo from './components/WhereToGo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WhereToGo
   }
 }
 </script>
